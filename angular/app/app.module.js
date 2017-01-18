364a3cc13aa04c97cee9e245c8b620269b1d0807
Paulo Penicheiro <smasher35@gmail.com>
2017-01-18 16:32:23 +0000

    correcções na foto do álvaro, para o layout ficar correcto adicionei um novo favicon

--- angular/app/app.module.js
+++ angular/app/app.module.js
@@ -5,9 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
@@ -34,8 +31,7 @@ AppModule = __decorate([
         declarations: [app_component_1.AppComponent, chat_component_1.ChatComponent, board_component_1.BoardComponent, about_component_1.AboutComponent, login_component_1.LoginComponent, signup_component_1.SignUpComponent, gamelobby_component_1.GameLobbyComponent, topten_component_1.TopTenComponent],
         providers: [websocket_service_1.WebSocketService],
         bootstrap: [app_component_1.AppComponent]
-    }),
-    __metadata("design:paramtypes", [])
+    })
 ], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
